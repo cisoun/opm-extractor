2559873f10849a063e92a7dbd87a4af9a19f9555
cisoun <cysoun@gmail.com>
2020-07-09 17:18:40 +0200

    Extract files into a directory

--- opm-extractor.py
+++ opm-extractor.py
@@ -7,11 +7,13 @@ Author: Cyriaque 'cisoun' Skrapits
 
 import argparse
 import base64
+import os
 from xml.dom import minidom
 
 
 def parse():
-    parser = argparse.ArgumentParser(description='Extract files from an OPM file.')
+    parser = argparse.ArgumentParser(
+        description='Extract files from an OPM file into a new directory.')
     parser.add_argument(
         'file',
         type=str,
@@ -21,18 +23,25 @@ def parse():
 
 
 def extract(path):
+    folder = os.path.splitext(path)[0]  # Remove extension (.opm).
+
     opm = minidom.parse(path)
     file_list = opm.getElementsByTagName('Filelist')
     files = file_list.item(0).getElementsByTagName('File')
 
+    # Extract files.
     for file in files:
-        location = file.getAttribute('Location')
-        name = location.replace('/', '.')
-        data = file.firstChild.data
+        file_path = folder + '/' + file.getAttribute('Location')
+        file_root = os.path.dirname(file_path)
 
-        print('Extracting %s' % name)
+        print('Extracting %s' % file_path)
 
-        with open(name, 'wb+') as f:
+        # Recreate file's directory.
+        os.makedirs(file_root, exist_ok=True)
+
+        # Recreate file.
+        with open(file_path, 'wb+') as f:
+            data = file.firstChild.data
             f.write(base64.b64decode(data))
 
 
